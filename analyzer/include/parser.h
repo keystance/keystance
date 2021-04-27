d81b2bd89022df77d1fe2538860d9b43ae9b6f16
Arin <aringrigoras@gmail.com>
2021-04-27 17:02:04 +0300

    parser.h

--- analyzer/include/parser.h
+++ analyzer/include/parser.h
@@ -7,7 +7,9 @@
 #include "../../keywords/C/keywords.h"
 
 
-
+/*C*/
+Cerrors c_parser_start(TokenList *list, const char *source);
+Cerrors c_check_semicolon(const char *buf);
 
 
 #endif //PARSER_H
--- errors/C/Cerrors.h
+++ errors/C/Cerrors.h
@@ -2,10 +2,14 @@
 #define CERRORS_H
 
 
-#define SYNTAX_ERROR 0
-#define RUNTIME_ERROR 1
-#define LINKER_ERROR 2
-#define LOGICAL_ERROR 3
-#define SEMATIC_ERROR 4
+enum _Cerrors{
+  SYNTAX_ERROR,
+  RUNTIME_ERROR,
+  //LINKER_ERROR,
+  LOGICAL_ERROR,
+  //SEMATIC_ERROR,
+  NO_ERROR,
+};
+typedef enum _Cerrors Cerrors;
 
 #endif //CERRORS_H
