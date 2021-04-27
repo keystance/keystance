fd65199456384f541b0ddc55d7a15c3445476793
Arin <aringrigoras@gmail.com>
2021-04-27 22:36:00 +0300

    util.c

--- analyzer/include/util.h
+++ analyzer/include/util.h
@@ -3,31 +3,6 @@
 
 #include "../../include/keystance.h"
 
-char *read_file(const char *path){
-  FILE *fptr;
-  fptr = fopen(path, "r");
-
-  if(!fptr){
-      printf("Couldn't open file %s", path);
-  }
-
-  fseek(fptr, 0, SEEK_END);
-  int size = ftell(fptr);
-  fseek(fptr, 0, SEEK_SET);
-
-  char *buffer = (char*)malloc(sizeof(char) * (size + 1));
-
-  if(!buffer){
-      printf("Couldn't allocate memory %d", __LINE__);
-      return NULL;
-  }
-
-  fread(buffer, 1, size, fptr);
-  buffer[size] = '\0';
-
-  fclose(fptr);
-
-  return buffer;
-}
+char *read_file(const char *path);
 
 #endif //UTIL_H
---
+++ analyzer/util.c
@@ -0,0 +1,28 @@
+#include "./include/util.h"
+
+char *read_file(const char *path){
+  FILE *fptr;
+  fptr = fopen(path, "r");
+
+  if(!fptr){
+      printf("Couldn't open file %s", path);
+  }
+
+  fseek(fptr, 0, SEEK_END);
+  int size = ftell(fptr);
+  fseek(fptr, 0, SEEK_SET);
+
+  char *buffer = (char*)malloc(sizeof(char) * (size + 1));
+
+  if(!buffer){
+      printf("Couldn't allocate memory %d", __LINE__);
+      return NULL;
+  }
+
+  fread(buffer, 1, size, fptr);
+  buffer[size] = '\0';
+
+  fclose(fptr);
+
+  return buffer;
+}
