966fd5392d8506a84e9fb2dbf800753d30ddda48
Arin <aringrigoras@gmail.com>
2021-04-27 15:17:05 +0300

    all of C's keywords

--- analyzer/include/parser.h
+++ analyzer/include/parser.h
@@ -1,6 +1,16 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "../../include/keystance.h"
 
+enum _TokenType{
+  KEYWORD,
+  DATA_TYPE,
+  COMMENT,
+  NUMBER,
+  FUNCTION,
+  SEMICOLON,
+};
+typedef enum _TokenType TokenType;
 
 #endif //PARSER_H
