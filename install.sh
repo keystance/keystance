d7e627b8298672145023148d97ee5798d1832622
Arin <aringrigoras@gmail.com>
2021-04-30 23:51:52 +0300

    version 0.3

---
+++ install.sh
@@ -0,0 +1,22 @@
+sudo apt install build-essential
+
+echo
+echo
+echo
+
+chmod +x ./main.bat
+./main.bat
+
+
+echo
+echo
+echo
+
+
+sudo cp ./kst /usr/local/bin
+echo "Intalling keystance editor..."
+echo
+
+
+
+echo "Done... To run the program write kst [filepath]"
---
+++ main.bat
@@ -0,0 +1,23 @@
+echo Copyright 2021 Arin Grigoras
+
+echo 
+
+echo Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files, 
+echo to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+echo and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+echo 
+
+echo The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+echo 
+
+echo THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+echo FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+echo WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+echo 
+echo 
+echo
+
+echo -Hello and thanks for using keystance for your text editing needs
