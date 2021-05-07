b305b7a44ca2abbb8790e2ec4e84ce5ecac1aed9
Arin <aringrigoras@gmail.com>
2021-05-07 14:51:04 +0300

    basic window going

---
+++ gui/include/keystance.h
@@ -0,0 +1,12 @@
+#ifndef KEYSTANCE_H
+#define KEYSTANCE_H
+
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+using namespace std;
+
+#endif //KEYSTANCE_H
